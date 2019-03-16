a40feeb4678a8892e8352b5d476a84b8568f91ea
Eduardo <correiaeduardojr@gmail.com>
2019-03-16 18:32:51 -0300

    Iniciando projeto de calculadora

---
+++ src/calculadoraims/Program.java
@@ -0,0 +1,17 @@
+package calculadoraims;
+
+/**
+ *
+ * @authors:
+ * Eduardo Correia
+ * Felipe Bastos
+ * Felipe Neiva
+ * Luiz Miguel Santana
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando calculadora...");
+    }
+    
+}
