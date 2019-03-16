d0a036dabc149777221087656e8e90c24b9547f8
Eduardo <correiaeduardojr@gmail.com>
2019-03-16 18:43:37 -0300

    Adicionado obtenção de operandos e operação

--- src/calculadoraims/Program.java
+++ src/calculadoraims/Program.java
@@ -1,5 +1,7 @@
 package calculadoraims;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @authors:
@@ -12,6 +14,27 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Iniciando calculadora...");
+        Object[] operacoes = {"Soma","Subtração","Multiplicação","Divisão"};
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro número."));
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo número."));
+        int operacaoEscolhida = JOptionPane.showOptionDialog(null, "EScolha a operação a ser realizada.", "Escolhendo operação...", 0, 2, null, operacoes, operacoes[0]);
+        switch(operacaoEscolhida){
+            case 0: // SOMA
+                
+                break;// FIM - SOMA
+                
+            case 1: // SUBTRAÇÃO
+                
+                break;// FIM - SUBTRAÇÃO
+                
+            case 2: // MULTIPLICAÇÃO
+                
+                break;// FIM - MULTIPLICAÇÃO
+                
+            case 3: // DIVISÃO
+                
+                break;// FIM - DIVISÃO
+        }
     }
     
 }
