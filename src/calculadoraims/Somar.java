7a126275b46134d8a9bb248b3b737a57810ba483
Eduardo <correiaeduardojr@gmail.com>
2019-03-28 09:53:52 -0300

    Adicionado a classe somar

---
+++ src/calculadoraims/Somar.java
@@ -0,0 +1,14 @@
+package calculadoraims;
+
+/**
+ * @authors:
+ * Eduardo Correia
+ * Felipe Bastos
+ * Felipe Neiva
+ */
+public class Somar {
+    public double somar(double x, double y){
+        return x+y;
+    }
+
+}
