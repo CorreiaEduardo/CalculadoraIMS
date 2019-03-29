4c8441e6e0f5a79f86de80ce1d8371875ee5dc0a
felipesncf <piocarvalho_1@hotmail.com>
2019-03-28 21:18:45 -0300

    Foi adicionado a funcao multiplicar

---
+++ src/calculadoraims/Multiplicar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraims;
+
+/**
+ *
+ * @author pioca_000
+ */
+public class Multiplicar {
+    public double multiplicar(double x, double y){
+        return x*y;
+    }
+}
